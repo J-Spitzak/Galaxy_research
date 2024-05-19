1eb889cd042d8b3696f038ef8e6fe5343c31f31c
Jason Spitzak <jason.spitzak@gmail.com>
2024-05-19 15:15:01 -0400

    finally works, just cleaning up some IO stuff

--- deproject.py
+++ deproject.py
@@ -2,29 +2,34 @@ import astropy.io
 from astropy.io import fits
 from astropy.wcs import WCS
 from astropy.nddata import Cutout2D
+#from astropy.modeling.rotations import Rotation2D
 from photutils.isophote import Ellipse, EllipseGeometry
 from photutils.aperture import EllipticalAperture
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
+from scipy.ndimage import rotate
+from scipy.ndimage import affine_transform, zoom
+from numpy import ndarray, array, double
 import math
 import sys
-import warnings
-warnings.filterwarnings("error")
+
 
 inputFile = sys.argv[1]
 bandLetter = input("band: ")
+
+custom = input("Create custom deprojection? (y/n):")
 
 fitsImage = fits.open(inputFile)
 
 data = fitsImage[0].data
 
-try:
+if custom == 'n':
     
     ellipse = Ellipse(data, None)
     isolist = ellipse.fit_image()
     print(isolist.pa)
 
-except RuntimeWarning:
+else:
     print("Creating custom fit:")
 
     plt.imshow(data,norm=LogNorm(), origin='lower')
@@ -61,11 +66,31 @@ except RuntimeWarning:
     hdu.data = cutout.data
     hdu.header.update(cutout.wcs.to_header())
     cutout_filename = (inputFile + "_" + str(bandLetter) + "_corrected")
-    hdu.writeto(cutout_filename, overwrite=True)
+    #hdu.writeto(cutout_filename, overwrite=False)
 
     newImage = fits.open(cutout_filename)
     newData = newImage[0].data
 
     plt.imshow(newData,norm=LogNorm(), origin='lower')
+    aper.plot(color='white')
     plt.show()
+
+    newData = rotate(input=newData, angle=(Angle * 180 / math.pi))
+    plt.imshow(newData,norm=LogNorm(), origin='lower')
+    plt.show()
+
+    factor = Major/Minor
+    #scaleMatrix = array([[1.0, 0.0, 0.0], [0.0, factor, 0.0], [0.0,0.0,1.0]])
+    #scaleMatrix = ndarray(shape=[3, 3], buffer=array([[[1/factor, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0,0.0,1.0]],[[1/factor, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0,0.0,1.0]]]),dtype=float)
+    #newData = affine_transform(input=newData, matrix=scaleMatrix)
+    newData = zoom(newData, (factor,1.0), order=0)
+
+    plt.imshow(newData,norm=LogNorm(), origin='lower')
+    plt.show()
+
+    if (input("write to file? (y/n):")=='y'):
+        newImage.data = newData
+        newImage.writeto(cutout_filename, overwrite=True)
+        print("written to at " + cutout_filename) 
+
     
