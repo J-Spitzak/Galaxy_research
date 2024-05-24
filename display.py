dfa7366f1b1e26c0c8e846a7929b1af2a6840ad1
Jason Spitzak <jason.spitzak@gmail.com>
2024-05-24 08:54:24 -0400

    did stuff with stuff

--- display.py
+++ display.py
@@ -7,23 +7,10 @@ rownum = 0
 
 gmanualPA = []
 gSFRavg = []
+gSFR100 = []
+gSFRFUV = []
 gname = []
 
-imanualPA = []
-iSFRavg = []
-iname = []
-
-rmanualPA = []
-rSFRavg = []
-rname = []
-
-umanualPA = []
-uSFRavg = []
-uname = []
-
-zmanualPA = []
-zSFRavg = []
-zname = []
 
 surveyfile = open("my_csv.csv",'r')	#open csv file with UG survey data
 
@@ -36,42 +23,21 @@ for row in csv.reader(surveyfile): 		#Read pairs file row by row
                 gSFRavg.append(float(row[8]))
             except:
                 gSFRavg.append(0)
-        elif (str(row[1]) == 'i'):
-            iname.append(str(row[0]).strip())
-            imanualPA.append(float(row[3]))        
             try:       
-                iSFRavg.append(float(row[8]))
+                gSFR100.append(float(row[6]))
             except:
-                iSFRavg.append(0)
-        elif (str(row[1]) == 'r'):
-            rname.append(str(row[0]).strip())
-            rmanualPA.append(float(row[3]))        
+                gSFR100.append(0)
             try:       
-                rSFRavg.append(float(row[8]))
+                gSFRFUV.append(float(row[7]))
             except:
-                rSFRavg.append(0)
-        elif (str(row[1]) == 'u'):
-            uname.append(str(row[0]).strip())
-            umanualPA.append(float(row[3]))        
-            try:       
-                uSFRavg.append(float(row[8]))
-            except:
-                uSFRavg.append(0)
-        elif (str(row[1]) == 'z'):
-            zname.append(str(row[0]).strip())
-            zmanualPA.append(float(row[3]))        
-            try:       
-                zSFRavg.append(float(row[8]))
-            except:
-                zSFRavg.append(0)
+                gSFRFUV.append(0)
     rownum = rownum + 1
 
 
 plt.scatter(gmanualPA,gSFRavg,color='blue')
-plt.scatter(imanualPA,iSFRavg,color='red')
-plt.scatter(rmanualPA,rSFRavg,color='green')
-plt.scatter(umanualPA,uSFRavg,color='purple')
-plt.scatter(zmanualPA,zSFRavg,color='black')
+plt.scatter(gmanualPA,gSFR100,color='red')
+plt.scatter(gmanualPA,gSFRFUV,color='green')
+
 
 plt.ylabel("SFR")
 plt.xlabel("Pitch angle")
