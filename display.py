87fbb1af0aad5ea05c464ccd5318c3ee0b174864
Jason Spitzak <jason.spitzak@gmail.com>
2024-08-17 22:34:10 -0400

    added sSFR measurement, legend  and line of best fit

--- display.py
+++ display.py
@@ -11,19 +11,21 @@ class band():
         self.SFRAVG = []
         self.SFR100 = []
         self.SFRFUV = []
+        self.sSFR = []
         self.name = []
     def addRow(self,row):
         try:       
-            a = float(row[8])
+            #a = float(row[8])
             b = float(row[6])
-            c = float(row[7])
+            #c = float(row[7])
         except:
             return
-        self.SFRAVG.append(float(row[8]))
+        #self.SFRAVG.append(float(row[8]))
         self.SFR100.append(float(row[6]))
-        self.SFRFUV.append(float(row[7]))
+        #self.SFRFUV.append(float(row[7]))
         self.manualPA.append(float(row[3]))
         self.name.append(str(row[0]).strip())
+        self.sSFR.append(float(row[6]) / (10 ** float(row[9])))
 
 gband = band('g')
 iband = band('i')
@@ -51,9 +53,13 @@ for row in csv.reader(surveyfile): 		#Read pairs file row by row
     rownum = rownum + 1
 
 
-plt.scatter(gband.manualPA,gband.SFR100,color='blue')
-plt.scatter(iband.manualPA,iband.SFR100,color='red')
-plt.scatter(rband.manualPA,rband.SFR100,color='green')
+plt.scatter(gband.manualPA,gband.SFR100,color='blue', label='G Band')
+plt.scatter(iband.manualPA,iband.SFR100,color='red', label='I Band')
+plt.scatter(rband.manualPA,rband.SFR100,color='green', label='R Band')
+plt.scatter(uband.manualPA,uband.SFR100,color='orange', label='U Band')
+plt.scatter(zband.manualPA,zband.SFR100,color='purple', label='Z Band')
+
+plt.plot(np.unique(iband.manualPA), np.poly1d(np.polyfit(iband.manualPA, iband.SFR100, 1))(np.unique(iband.manualPA)))
 
 """ plt.scatter(zband.manualPA,zband.SFRAVG,color='purple')
 plt.scatter(uband.manualPA,uband.SFRAVG,color='black')
@@ -65,5 +71,7 @@ plt.scatter(gband.manualPA,gband.SFRAVG,color='blue') """
 
 plt.ylabel("SFR")
 plt.xlabel("Pitch angle")
+
+plt.legend()
 
 plt.show()
